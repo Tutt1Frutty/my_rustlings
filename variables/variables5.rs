c0807789d51fbc969672bb0abaffa27e2323a35d
Maksym Zhersh <160425584+Tutt1Frutty@users.noreply.github.com>
2024-11-14 13:50:43 +0200

    second part

---
+++ variables/variables5.rs
@@ -0,0 +1,11 @@
+// variables5.rs
+// Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
+
+// I AM DONE
+
+fn main() {
+    let number = "T-H-R-E-E"; // don't change this line
+    println!("Spell a Number : {}", number);
+    let int_number = 3;
+    println!("Number plus two is : {}", int_number + 2);
+}
